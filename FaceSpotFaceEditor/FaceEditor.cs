b82bb43368d23a4c5b02202f0de19edf49fe9a0d
kanitw <kanitw@gmail.com>
2009-11-25 23:41:59 +0700

    Setting Up Project Try To Start Working

---
+++ FaceSpotFaceEditor/FaceEditor.cs
@@ -0,0 +1,36 @@
+
+using System;
+using FSpot.Editors;
+using Mono.Unix;
+
+namespace FaceSpot
+{
+	public class FaceEditor : Editor
+	{
+
+		public FaceEditor () : base (Catalog.GetString ("Face"), null)
+		{
+			CanHandleMultiple = false;
+			NeedsSelection = true;
+			// figure this out!
+			HasSettings = true;
+			
+			ApplyLabel = Catalog.GetString("Apply");
+			Initialized += delegate { State.PhotoImageView.PhotoChanged += delegate { UpdateSelectionCombo (); }; };
+		}
+		
+		protected override Gdk.Pixbuf Process (Gdk.Pixbuf input, Cms.Profile input_profile)
+		{
+			throw new System.NotImplementedException ();
+		}
+		
+		public override Gtk.Widget ConfigurationWidget ()
+		{
+			VBox vbox = new VBox();
+			Label faceFound = 
+			return vbox;
+		}
+
+
+	}
+}
