2a0c19246d4567bb2d24a85f2e721613f3b912db
kanitw <kanitw@gmail.com>
2009-12-07 15:06:07 +0700

    Edit a part of Facebrowser glade also add FaceWidget.cs

---
+++ FaceSpot/ui/FaceWidget.cs
@@ -0,0 +1,23 @@
+
+using System;
+using FaceSpot.Db;
+
+namespace FaceSpot
+{
+
+
+	public class FaceWidget
+	{
+		/// <summary>
+		/// Show Whole Picture Thumbnails or only faces
+		/// </summary>
+		bool showOnlyFaces = true;
+		Face face;
+		
+		public FaceWidget ()
+		{
+		}
+		
+		
+	}
+}
